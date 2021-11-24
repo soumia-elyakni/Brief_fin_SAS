bd9739ea64f68e88bd32359c12094e3ef0f35250
soumia elyakni <selyakni@gmail.com>
2021-11-24 15:12:14 +0100

    merc312pm

--- Menu.c
+++ Menu.c
@@ -11,7 +11,7 @@ struct info_client{
  };
 
  struct info_client Client;
- 
+
  void ajouteruncmpte(){
 
 
@@ -53,7 +53,7 @@ int main(){
  
 int choix;
 
- printf("Bienvenue sur le systeme bancaire");
+ printf("*******************Bienvenue sur le systeme bancaire******************** \n");
     
     menu :
     printf("1-Ajouter un client\n");
--- recherche1clt.c
+++ recherche1clt.c
@@ -1,1 +1,19 @@
 ************************recherche un Client***********************
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void searching ()
+{
+
+   char recherche[9];
+    
+   printf("entrer le CIN du client que voulez-vous trouver");
+   scanf("%s", recherche );
+   
+   strstr(recherche, Client.CIN);
+
+
+
+}
+   
